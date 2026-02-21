c5f16cd0dbff97e29d4189f57af1256e3a3661cd
hoanganhry <159603492+hoanganhry@users.noreply.github.com>
2026-02-21 14:06:26 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API running successfully" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
